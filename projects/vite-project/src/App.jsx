4e1ca74fb51308b72088ea9328600cc489544341
Yeisson19 <yeissoncolmenarez@gmail.com>
2023-12-14 19:13:20 -0400

    two commit

---
+++ projects/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+export function App(){
+    return(
+   
+   <>
+        <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán" />
+   </>
+
+    )
+}
